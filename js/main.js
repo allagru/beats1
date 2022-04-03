d6c14f7eb1cb7b1c37a47800733684ccf7943483
allagru <58115081+allagru@users.noreply.github.com>
2022-04-03 14:18:08 +0300

    внесены исправления 3 недели

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
+// let burger = document.querySelector(".burger");
+// let hideMenu = document.querySelector(".hide-menu");
+// let hideMenuClose = document.querySelector(".hide-menu__close");
+
+// burger.addEventListener("click", function(event){
+//     if(event){
+//         hideMenu.style.display = "block";
+//     }  
+// })
+
+// hideMenuClose.addEventListener("click", function(event){
+//     if(event){
+//         hideMenu.style.display = "none";
+//     }
+// })
 let burger = document.querySelector(".burger");
 let hideMenu = document.querySelector(".hide-menu");
 let hideMenuClose = document.querySelector(".hide-menu__close");
 
 burger.addEventListener("click", function(event){
-    if(event){
-        hideMenu.style.display = "block";
-    }  
+    event.preventDefault();
+    hideMenu.classList.add('hide-menu--active');
 })
 
 hideMenuClose.addEventListener("click", function(event){
-    if(event){
-        hideMenu.style.display = "none";
-    }
+    event.preventDefault();
+    hideMenu.classList.remove('hide-menu--active');
 })
-
